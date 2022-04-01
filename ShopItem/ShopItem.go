1d194c8626bb682eb0c0c7fab365e4aabf3a7beb
jack yao <hyson007@gmail.com>
2022-04-01 10:28:07 +0800

    refactor in cast

---
+++ ShopItem/ShopItem.go
@@ -0,0 +1,16 @@
+package shopitem
+
+import "fmt"
+
+type shopItemHelper struct {
+	Category int     `json:"category"`
+	Quantity int     `json:"quantity"`
+	Cost     float64 `json:"cost"`
+	Name     string  `json:"name"`
+}
+
+func (s shopItemHelper) String() string {
+
+	return fmt.Sprintf("%s - %v\n", item, value)
+
+}
